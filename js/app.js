4d01746d40fa130114c763edced9065ef83544be
Otavio Araujo <otavioajardim@gmail.com>
2019-07-20 11:59:28 -0300

    set menu toggle button

---
+++ js/app.js
@@ -0,0 +1,6 @@
+ var alternator = document.querySelector('.js-toggle');
+
+ alternator.onclick = function() {
+ 	var menu = document.querySelector('.js-menu');
+ 	menu.classList.toggle('menu--showing');
+ }
